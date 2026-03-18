b61b1d41f0e7b33d08e7467e75f52238ec78d2b5
Rubinich <alojzrubinic@gmail.com>
2026-03-18 16:36:29 +0100

    first commit

---
+++ vjezba1/vjezba1.php
@@ -0,0 +1,11 @@
+<?php
+$naslov = "Moja prva web aplikacija";
+$autor = "Alojz Rubinić";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu je izradio <strong>$autor</strong>.</p>";
+echo '<a href="https://www.mojaplaca.hr/" target="_blank">Koja je moja plaća?</a>';
+
+?>
+
+<!--Naziv datoteke: vjezba1.php-->
